3b4d9d6d406c426adf999c526ed9d1befcdbc43c
vishalP143 <vishal.p8070@gmail.com>
2024-12-18 07:37:54 +0000

    ok

--- config/db.js
+++ config/db.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
 
 const connectDB = async () => {
   try {
-    // Connect to MongoDB without deprecated options
-    await mongoose.connect('mongodb://localhost:27017/mydatabase');
-    console.log('Connected to MongoDB');
-  } catch (err) {
-    console.error('Failed to connect to MongoDB', err);
-    process.exit(1); // Exit the process with failure
+      const DB = process.env.MONGO_ATLAS_URL;
+      console.log('Connecting to:', DB); // Log the URI for debugging
+
+      const conn = await mongoose.connect(DB);
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+      console.error(`Error: ${error.message}`);
+      process.exit(1); // Exit with failure
   }
 };
 
--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
+const cors = require('cors');
+const dotenv = require("dotenv")
+
 const connectDB = require('./config/db'); // Import the database connection function
 const roomRoutes = require('./routes/roomRoutes'); // Import room routes
+dotenv.config({ path: "./config.env"})
+
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-const cors = require('cors');
-app.use(cors());
 
 // Connect to MongoDB
 connectDB();
 
 app.use(express.json()); // Middleware to parse JSON requests
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("HomePage Of The Rental Management App"); // Home page route
@@ -19,6 +21,7 @@ app.get("/", (req, res) => {
 
 app.use('/api', roomRoutes); // Use room routes with prefix '/api'
 
+const PORT = process.env.PORT || 5000;
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
